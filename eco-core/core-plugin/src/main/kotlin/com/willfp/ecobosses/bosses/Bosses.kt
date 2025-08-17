019c97d045fc4168088323f4a7e11d85260ee23b
MMN <64864304+MMN-2024@users.noreply.github.com>
2025-08-17 18:47:23 +0200

    Fix config loading with correct eco API

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobosses/bosses/Bosses.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobosses/bosses/Bosses.kt
@@ -2,7 +2,8 @@ package com.willfp.ecobosses.bosses
 
 import com.google.common.collect.ImmutableList
 import com.willfp.eco.core.registry.Registry
-import com.willfp.eco.core.config.updating.ConfigUpdater
+import com.willfp.eco.core.config.BaseConfig
+import com.willfp.eco.core.config.ConfigType
 import com.willfp.ecobosses.EcoBossesPlugin
 import org.bukkit.entity.Entity
 import org.bukkit.entity.LivingEntity
@@ -30,7 +31,19 @@ object Bosses {
                 loadBossesFromDirectory(file, plugin)
             } else if (file.extension == "yml" && !file.name.startsWith("_")) {
                 try {
-                    val config = ConfigUpdater.loadConfig(file, plugin.javaClass)
+                    val config = object : BaseConfig(
+                        file.nameWithoutExtension,
+                        plugin,
+                        ConfigType.YAML,
+                        false
+                    ) {
+                        init {
+                            // Load the file content
+                            if (file.exists()) {
+                                this.load(file.inputStream())
+                            }
+                        }
+                    }
                     val id = file.nameWithoutExtension
                     registry.register(EcoBoss(id, config, plugin))
                 } catch (e: Exception) {
